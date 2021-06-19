b5737986a190e2fe4a4b5888b0ec12dec64350ef
Alan-Biju <alanbiju238@gmail.com>
2021-06-19 16:56:30 +0530

    touch select error

--- src/Components/NavBar.js/NavBar.js
+++ src/Components/NavBar.js/NavBar.js
@@ -108,6 +108,8 @@ const Icon = styled(NavLink)`
 	transition: color 0.5s ease;
 	position: relative;
 	user-select: none;
+	-webkit-touch-callout: none;
+	-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
 	&:hover {
 		color: #1a1919;
 	}
